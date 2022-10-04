225e23fcaae09436cbbf0d0973426a19e02ae8e5
James Tran <tran.james2001@gmail.com>
2022-10-03 21:48:40 -0400

    Handle case when image has not been generated

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import aioboto3
+import botocore
 
 from uuid import uuid4
 from io import BytesIO
@@ -31,16 +32,22 @@ async def generate_image(rq: GenerateImageRequest):
 
     return GenerateImageResponse(request_id=request_id)
 
-@app.get("/results/{request_id}", response_model=GetResultsResponse)
+@app.get("/results/{request_id}")
 async def get_results(request_id: str):
     obj_key = f"{request_id}_generated_image"
     buffer = BytesIO()
 
     async with aioboto3.Session().resource("s3") as s3:
         bucket = await s3.Bucket("fsdl-power-rangers")
-        await bucket.download_fileobj(obj_key, buffer)
+        try:
+            await bucket.download_fileobj(obj_key, buffer)
+        except botocore.exceptions.ClientError as err:
+            if err.response['Error']['Code'] == "404":
+                return {}
+            else:
+                raise err
 
     base64_img = base64.b64encode(
         buffer.getvalue()
     )
-    return GetResultsResponse(generated_image=str(base64_img.decode()))
+    return { "generated_image": str(base64_img.decode()) }
--- app/response_schemas/get_results.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class GetResultsResponse(BaseModel):
-    generated_image: str
