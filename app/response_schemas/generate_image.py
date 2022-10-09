759f1b96f79474e633ce71846cb77d4c703414cf
David Kim <david@Davids-Air.lan>
2022-10-09 01:49:20 -0400

    update get request

--- app/response_schemas/generate_image.py
+++ app/response_schemas/generate_image.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
 
+class SimilarityResult(BaseModel):
+    id: str
+    caption: str
+    url: str
+    similarity: float
 
 class GenerateImageResponse(BaseModel):
-    request_id: str
+    message_id: str,
+    gen_image: str,
+    similarity_results: List[SimilarityResult]
